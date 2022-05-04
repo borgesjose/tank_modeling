c547e8260339e05c428a233bdc51db7af85c71b6
Zb8 <jose.borges90@hotmail.com>
2022-05-04 19:50:46 -0300

    04-05-2022 version

--- simula_tanque_cilindrico.m
+++ simula_tanque_cilindrico.m
@@ -9,53 +9,63 @@
 %  -- Version: 1.0  - 02/05/2022                      %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
 %Passo 1, definir o vetor tempo:
-    Ts = 10; % tempo de amostragem para processo de um tanque ( Landau,2006)
-    nptos = 1000;
-    ts = linspace(0,Ts,nptos+1);
+    Ts = .010; % periodo de amostragem para processo de um tanque ( Landau,2006)
+    Tsim = 30 
+    nptos = Tsim/Ts;
+    ts = linspace(0,Tsim,nptos+1);
     
-
 %% Passo 2 - Definições:
-nptos
-SP = 1;
-Level0 = 0.1;
-c = 50.0;          % valve coefficient (kg/s / %open)
-u = zeros(101,1); % u = valve % open
+
+%Dados do probelma:
+
+h0 = 0.7; % ponto inicial
+
+u = zeros(nptos+1,1); % variavel de entrada
+h = zeros(nptos+1,1); % variavel de saida
 
 Cv = 0.97 %velocity coefficient (water 0.97)
 Cc = 0.97 %contraction coefficient (sharp edge aperture 0.62, well rounded aperture 0.97)
 
-Cd = Cc Cv % discharge coefficient
+Cd = Cc*Cv % discharge coefficient
 
-A = 9.269; % Constante da valvula
+r = 0.01;% raio do orificio de saida em metros
+
+A = pi*r^2;% Area do orificio de saida
 
 %%
- for i=1:nptos,
-    if (i<=nptos/5)  ref(i)=30; end;
-    if (i>nptos/5)   ref(i) = 50; end;
+% definindo a referencia de controle 
+for i=1:nptos+1,
+    if (i<=nptos/5)  ref(i)=.5; end;
+    if (i>nptos/5)   ref(i) = .5; end;
 end ;
 
-
+% Calculando o input
+ for i=1:nptos, 
+    %if (i<=nptos/5)  u(i)=.0005; end;
+    %if (i>nptos/5)   u(i) = .0005; end;
+    u(i)=.0015;
+end ;
+[0 0.1]
 % Simulation with ode45;
 for i=1:nptos
     
-    u(i+1) = ref(i);   % store the valve position
+    %u(i+1) = ref(i);   % store the Qin value
    
-    [~,y] = ode45(@(t,y) tank_cilindrical(t,y,u(i)),[0 0.1],Level0);
-    Level0 = y(end); % take the last point
-    z(i+1) = Level0; % store the level for plotting
-    sps(i+1) = ref(i);
+    [~,y] = ode45(@(t,y) tank_cilindrical(t,y,A,u(i)),[0,0.1],h0);
+    %if (y<=0) y=0;end;
+    %if (y>=.7) y=.7; end;
+    %[~,rr] = y;
+    h0 = y(end); % take the last point
+    h(i+1) = h0; % store the height for plotting
+    if (y<=0) h(i+1)=0;end;
+    if (y>=.7) h(i+1)=.7; end;
+
 end
-
-
+%%
 % plot results
-subplot(3,1,1)
-plot(ts,z,'-r','LineWidth', 3,'DisplayName','level PV'); hold on
-plot(ts,sps,'k:','LineWidth', 3,'DisplayName','level SP'); hold off
-ylabel('Tank Level');
+figure;
+plot(ts,h,'-r','LineWidth', 3,'DisplayName','height'); hold on
+plot(ts,ref,'k:','LineWidth', 3,'DisplayName','input'); hold off
+ylabel('Tank Height');
 legend();
-
-
-
-
