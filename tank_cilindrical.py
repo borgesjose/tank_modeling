ad71c1582cf480efa14878fe5a35be033bffbe98
borgesjose <jose.borges90@hotmail.com>
2022-05-10 23:15:38 -0300

    10-05-2022 version

---
+++ tank_cilindrical.py
@@ -0,0 +1,15 @@
+def dh_dt(t,y,A,Qin,Cd):
+    # calculate derivative of the Level
+    
+    g = 9.81;
+    R = 0.042; % Raio em metros
+    
+    b = A*Cd*sqrt(2*g);
+    
+    k1 = 1/(pi*(R)^2);
+    k2 = b/(pi*(R)^2);
+
+
+    dh_dt = Qin*k1 - k2*y**(1/2);
+
+    return dh_dt
